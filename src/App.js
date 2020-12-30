9e8afb17467df44b3432f417a9a57cf4ae1582d7
Chris Phong <thong20@Chriss-MacBook-Pro.local>
2020-12-30 11:00:13 +0700

    20201230-110000

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-
 import './App.css';
+import { Button } from 'react-bootstrap';
+
 
 function App() {
   return (
     <div className="App">
       <h1>App Component</h1>
+      <Button onClick={() => console.log('clicked')}>Show Toast</Button>
     </div>
   );
 }
