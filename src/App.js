843bf117ea58dd6bc48c085c311823c9ffb29adb
Chris Phong <thong20@Chriss-MacBook-Pro.local>
2020-12-30 12:27:49 +0700

    20201230-122711

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
-import './App.css';
-import { Button } from 'react-bootstrap';
-
+import "./App.css";
+import { Button, Col, Container, Row } from "react-bootstrap";
 
 function App() {
   return (
     <div className="App">
-      <h1>App Component</h1>
-      <Button onClick={() => console.log('clicked')}>Show Toast</Button>
+      <Container className="_container">
+        <Row>
+          <Col className="item">Header</Col>
+        </Row>
+        <Row>
+          <Col xs={4} className="item" align-middle>
+            <Button variant="primary">Menu</Button>
+          </Col>
+          <Col>
+            <Row>
+              <Col className="item">Content</Col>
+              <Col className="item">Right</Col>
+            </Row>
+            <Row>
+              <Col className="item">Footer</Col>
+            </Row>
+          </Col>
+        </Row>
+        
+      </Container>
     </div>
   );
 }
