b9a34535b2f32bf5969c7d0f289a0d4d3ea1d9a1
thong20 <minhphong1515@gmail.com>
2021-01-17 18:46:35 +0700

    install bootstrap reactstrap

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 
 import './App.css';
+import Form from './screens/Form'
 
 function App() {
   return (
     <div className="App">
       <h1>App Component</h1>
+      <Form />
     </div>
   );
 }
