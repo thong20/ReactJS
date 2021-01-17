080c0bd42fc7840d92332cf16f8a755fa784d9a0
thong20 <minhphong1515@gmail.com>
2021-01-17 19:47:13 +0700

    cleanup

--- src/screens/Form.js
+++ src/screens/Form.js
@@ -1,58 +1,55 @@
 import React, { useState } from "react";
 import PropTypes from "prop-types";
 import {
-  Button, Container, Row, Col,
-  Card, CardBody, CardImg, CardTitle, CardSubtitle, CardText
-} from 'reactstrap';
-
-
+  Button,
+  Form,
+  FormGroup,
+  FormText,
+  Label,
+  Input,
+  Container,
+  Row,
+  Col,
+} from "reactstrap";
 
 TodoApp.propTypes = {
   // name_prop: PropTypes.type
   todos: PropTypes.func, // passed from to binding/TodoApp.js
-  addTodo: PropTypes.func // passed from to binding/TodoApp.js
+  addTodo: PropTypes.func, // passed from to binding/TodoApp.js
 };
 
 function TodoApp(props) {
   const [text, setText] = useState("");
   return (
-    <Container>
+    <Container className='myCell'>
       <Row>
-        <Col className="myCell">.col</Col>
+        <Form>
+          <FormGroup row>
+            <Label for="exampleEmail" sm={2}>
+              Email
+            </Label>
+            <Col sm={10}>
+              <Input
+                type="email"
+                name="email"
+                id="exampleEmail"
+                placeholder="with a placeholder"
+              />
+            </Col>
+          </FormGroup>
+          <FormGroup check row>
+            <Col sm={{ size: 10, offset: 2 }}>
+              <Button>Submit</Button>
+            </Col>
+          </FormGroup>
+        </Form>
       </Row>
+      <Row ></Row>
       <Row>
-        <Col className="myCell">.col</Col>
-        <Col className="myCell">.col</Col>
-        <Col className="myCell">.col</Col>
-        <Col className="myCell">.col</Col>
-      </Row>
-      <Row>
-        <Col className="myCell" xs="3">.col-3</Col>
-        <Col className="myCell" xs="auto">.col-auto - variable width content</Col>
-        <Col className="myCell" xs="3">.col-3</Col>
-      </Row>
-      <Row>
-        <Col className="myCell" xs="6">.col-6</Col>
-        <Col className="myCell" xs="6">.col-6</Col>
-      </Row>
-      <Row>
-        <Col className="myCell" xs="6" sm="4">.col-6 .col-sm-4</Col>
-        <Col className="myCell" xs="6" sm="4">.col-6 .col-sm-4</Col>
-        <Col className="myCell" sm="4">.col-sm-4</Col>
-      </Row>
-      <Row>
-        <Col className="myCell" sm={{ size: 6, order: 2, offset: 1 }}>.col-sm-6 .order-sm-2 .offset-sm-1</Col>
-      </Row>
-      <Row>
-        <Col className="myCell" sm="12" md={{ size: 6, offset: 3 }}>.col-sm-12 .col-md-6 .offset-md-3</Col>
-      </Row>
-      <Row>
-        <Col className="myCell" sm={{ size: 'auto', offset: 1 }}>.col-sm-auto .offset-sm-1</Col>
-        <Col className="myCell" sm={{ size: 'auto', offset: 1 }}>.col-sm-auto .offset-sm-1</Col>
+        <p>lakjsdlfjalsdkjflkjlkj</p>
       </Row>
     </Container>
   );
 }
-
 
 export default TodoApp;
