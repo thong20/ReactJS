c9fd94636bf2797dbc2dc56162c7fd141763e9c4
Minh Phong <minhphong1515@gmail.com>
2022-04-12 01:01:13 -0700

    Update About.js

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -90,7 +90,9 @@ export default function About(){
               <div className="desc2 slider">
                 There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text.
               </div>
-              <button className="btn-download slider">
+              <button className="btn-download slider" onClick={() => {
+                window.open("https://drive.google.com/file/d/1-HBxxo-Fk791dsJpcSrO39NHCebBzykq/view?usp=sharing")
+              }}>
                 <span>Download CV</span>
               </button>
             </div>
